dd87ee18ec138dcf3c4701eac00793c2d1db6896
teddyedwar29 <teddyedwar@gmail.com>
2024-12-04 22:02:10 +0700

    membuat tugas file python

---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+from streamlit_option_menu import option_menu
+import home ,upload
+
+
+class MultiApp:
+
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run():
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='Pondering ',
+                options=['Home','Upload'],
+                icons=['house-fill','person-circle'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'black'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
+                
+                )
+
+        
+        if app == "Home":
+            home.app()
+        if app == "Upload":
+            upload.app()    
+    run()
